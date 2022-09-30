353bdcdab71430173db9f3475202d6db17b60adc
titoeb <timtoebrock@googlemail.com>
2022-09-30 05:45:08 +0000

    feat: Convert workout to string in crm-format.

--- src/workout_data/workout.rs
+++ src/workout_data/workout.rs
@@ -26,8 +26,12 @@ where
             efforts,
         }
     }
+    /// Generate the crm representation of a workout.
+    pub fn to_crm(&self) -> String {
+        format!("{}\n{}", self.crm_head(), self.crm_body())
+    }
 
-    fn crm_header(&self) -> String {
+    fn crm_head(&self) -> String {
         format! {
             "[COURSE HEADER]\n\
             DESCRIPTION = {}\n\
@@ -36,6 +40,26 @@ where
             self.description,
             EffortType::generate_effort_header()
         }
+    }
+    fn crm_body(&self) -> String {
+        format!(
+            "[COURSE DATA]\n\
+            {}\n\
+            [END COURSE DATA]",
+            self.crm_body_workouts()
+        )
+    }
+    fn crm_body_workouts(&self) -> String {
+        let mut efforts_as_crm = Vec::new();
+        let mut current_starting_minute = PositiveFloat::new(0.0).unwrap();
+
+        for effort in &self.efforts {
+            let (effort_as_crm, new_starting_minute) = effort.to_crm(current_starting_minute);
+            efforts_as_crm.push(effort_as_crm);
+            current_starting_minute = new_starting_minute;
+        }
+
+        efforts_as_crm.join("\n")
     }
 }
 
@@ -56,6 +80,64 @@ where
         /// The individual efforts.
         efforts: Vec<EffortUnit<EffortType>>,
     },
+}
+
+impl<EffortType> Effort<EffortType>
+where
+    EffortType: ConvertEffortToCRM + GenerateCRMEffortHeader,
+{
+    fn to_crm(&self, starting_minute: PositiveFloat) -> (String, PositiveFloat) {
+        match self {
+            Self::SingleEffort(single_effort) => single_effort.to_crm(starting_minute),
+            Self::GroupEffort { efforts } => efforts_to_crm(efforts, &starting_minute),
+        }
+    }
+}
+
+fn efforts_to_crm<EffortType>(
+    efforts: &Vec<EffortUnit<EffortType>>,
+    starting_minute: &PositiveFloat,
+) -> (String, PositiveFloat)
+where
+    EffortType: GenerateCRMEffortHeader + ConvertEffortToCRM,
+{
+    let starting_minutes = extract_initial_starting_minutes(efforts, starting_minute);
+    let effort_string_with_final_minute = efforts
+        .iter()
+        .zip(starting_minutes.into_iter())
+        .map(|(effort, starting_minute)| effort.to_crm(starting_minute))
+        .collect::<Vec<(String, PositiveFloat)>>();
+
+    (
+        effort_string_with_final_minute
+            .iter()
+            .map(|(effort_string, _)| effort_string.clone())
+            .collect::<Vec<String>>()
+            .join("\n"),
+        effort_string_with_final_minute
+            .last()
+            .unwrap_or(&(String::from(""), starting_minute.clone()))
+            .1
+            .clone(),
+    )
+}
+
+fn extract_initial_starting_minutes<EffortType>(
+    efforts: &Vec<EffortUnit<EffortType>>,
+    starting_minute: &PositiveFloat,
+) -> Vec<PositiveFloat>
+where
+    EffortType: GenerateCRMEffortHeader + ConvertEffortToCRM,
+{
+    let mut starting_times = Vec::new();
+    let mut current_starting_time = starting_minute.clone();
+
+    for effort in efforts {
+        starting_times.push(current_starting_time.clone());
+        current_starting_time = current_starting_time + effort.duration_in_minutes.clone();
+    }
+
+    starting_times
 }
 
 /// Combining a type of effort with a duration
@@ -83,11 +165,15 @@ where
     fn effort_in_crm(&self) -> String {
         self.effort.to_crm()
     }
-    fn to_crm(&self, starting_minute: PositiveFloat) -> String {
-        format! {
-            "{}\t{}\n\
-            {}\t{}", starting_minute.to_crm(), self.effort_in_crm(), starting_minute.add(&self.duration_in_minutes).to_crm(), self.effort_in_crm()
-        }
+    fn to_crm(&self, starting_minute: PositiveFloat) -> (String, PositiveFloat) {
+        let end_of_effort = starting_minute.clone() + self.duration_in_minutes.clone();
+        (
+            format! {
+                "{}\t{}\n\
+                {}\t{}", starting_minute.to_crm(), self.effort_in_crm(), end_of_effort.to_crm(), self.effort_in_crm()
+            },
+            end_of_effort,
+        )
     }
 }
 /// This function is used to create the correct Header in
@@ -209,11 +295,42 @@ mod test {
                 Workout::new("test_workout", "Workout for testing", vec![]);
 
             assert_eq!(
-                workout.crm_header(),
+                workout.crm_head(),
                 "[COURSE HEADER]\n\
             DESCRIPTION = Workout for testing\n\
             MINUTES EFFORT-UNIT\n\
             [END COURSE HEADER]"
+            )
+        }
+
+        #[test]
+        fn workout_to_crm() {
+            assert_eq!(
+                Workout::new(
+                    "test_workout",
+                    "test-1",
+                    vec![
+                        Effort::SingleEffort(EffortUnit::new(
+                            PositiveFloat::new(5.0).unwrap(),
+                            Watts::new(PositiveFloat::new(80.0).unwrap()),
+                        )),
+                        Effort::SingleEffort(EffortUnit::new(
+                            PositiveFloat::new(10.0).unwrap(),
+                            Watts::new(PositiveFloat::new(100.0).unwrap()),
+                        )),
+                    ],
+                )
+                .to_crm(),
+                "[COURSE HEADER]\n\
+                DESCRIPTION = test-1\n\
+                MINUTES WATTS\n\
+                [END COURSE HEADER]\n\
+                [COURSE DATA]\n\
+                0.00\t80.00\n\
+                5.00\t80.00\n\
+                5.00\t100.00\n\
+                15.00\t100.00\n\
+                [END COURSE DATA]"
             )
         }
     }
@@ -253,7 +370,9 @@ mod test {
         }
     }
     mod effort_unit {
-        use super::super::{EffortUnit, PercentOfFTP};
+        use super::super::{
+            efforts_to_crm, extract_initial_starting_minutes, EffortUnit, PercentOfFTP, Watts,
+        };
         use super::TestActivity;
         use crate::workout_data::positive_float::PositiveFloat;
 
@@ -281,14 +400,70 @@ mod test {
             assert_eq!(
                 EffortUnit::new(PositiveFloat::new(5.0).unwrap(), TestActivity {})
                     .to_crm(PositiveFloat::new(5.0).unwrap()),
-                "5.00	100.00\n\
+                (
+                    String::from(
+                        "5.00	100.00\n\
                 10.00	100.00"
+                    ),
+                    PositiveFloat::new(10.0).unwrap()
                 )
+            )
         }
 
+        #[test]
+        fn test_extract_starting_minutes_from_efforts() {
+            assert_eq!(
+                extract_initial_starting_minutes(
+                    &vec![
+                        EffortUnit::new(PositiveFloat::new(7.0).unwrap(), TestActivity {}),
+                        EffortUnit::new(PositiveFloat::new(9.0).unwrap(), TestActivity {})
+                    ],
+                    &PositiveFloat::new(5.0).unwrap()
                 ),
-                "5.00	100.00\n\
-                10.00	100.00"
+                vec![
+                    PositiveFloat::new(5.0).unwrap(),
+                    PositiveFloat::new(12.0).unwrap()
+                ]
+            )
+        }
+
+        #[test]
+        fn test_efforts_to_crm() {
+            assert_eq!(
+                efforts_to_crm(
+                    &vec![
+                        EffortUnit::new(
+                            PositiveFloat::new(5.0).unwrap(),
+                            Watts::new(PositiveFloat::new(100.0).unwrap())
+                        ),
+                        EffortUnit::new(
+                            PositiveFloat::new(10.0).unwrap(),
+                            Watts::new(PositiveFloat::new(150.0).unwrap())
+                        ),
+                        EffortUnit::new(
+                            PositiveFloat::new(15.0).unwrap(),
+                            Watts::new(PositiveFloat::new(200.0).unwrap())
+                        ),
+                        EffortUnit::new(
+                            PositiveFloat::new(5.0).unwrap(),
+                            Watts::new(PositiveFloat::new(120.0).unwrap())
+                        ),
+                    ],
+                    &PositiveFloat::new(5.0).unwrap()
+                ),
+                (
+                    String::from(
+                        "5.00\t100.00\n\
+                10.00\t100.00\n\
+                10.00\t150.00\n\
+                20.00\t150.00\n\
+                20.00\t200.00\n\
+                35.00\t200.00\n\
+                35.00\t120.00\n\
+                40.00\t120.00"
+                    ),
+                    PositiveFloat::new(40.0).unwrap()
+                )
             )
         }
     }
--- tests/workout_to_crm_string.rs
+++ tests/workout_to_crm_string.rs
@@ -3,72 +3,64 @@ use crm_workout_creator::workout_data::workout::{
     Effort, EffortUnit, PercentOfFTP, Watts, Workout,
 };
 
-// #[test]
-// fn simple_watt_workout_to_crm() {
-//     assert_eq!(
-//         Workout::new(
-//             "test_workout",
-//             "test-1",
-//             vec![
-//                 Effort::SingleEffort(EffortUnit::new(
-//                     PositiveFloat::new(5.0).unwrap(),
-//                     Watts::new(
-//                         PositiveFloat::new(80.0).unwrap()
-//                     ),
-//                 )),
-//                 Effort::SingleEffort(EffortUnit::new(
-//                     PositiveFloat::new(5.0)..unwrap(),
-//                     Watts::new(
-//                         PositiveFloat::new(80.0)..unwrap()
-//                     ),
-//                 )),
-//             ],
-//         )
-//         .to_crm(),
-//         "[COURSE HEADER]\n\
-//     DESCRIPTION = test-1\n\
-//     MINUTES PERCENTAGE\n\
-//     [END COURSE HEADER]\n\
-//     [COURSE DATA]\n\
-//     0.00	80.00\n\
-//     5.00	80.00\n\
-//     5.00	100.00\n\
-//     10.00	100.00\n\
-//     [END COURSE DATA]"
-//     )
-// }
+#[test]
+fn simple_percent_of_ftp_workout_to_crm() {
+    assert_eq!(
+        Workout::new(
+            "test_workout",
+            "test-1",
+            vec![
+                Effort::SingleEffort(EffortUnit::new(
+                    PositiveFloat::new(5.0).unwrap(),
+                    PercentOfFTP::new(PositiveFloat::new(80.0).unwrap()),
+                )),
+                Effort::SingleEffort(EffortUnit::new(
+                    PositiveFloat::new(5.0).unwrap(),
+                    PercentOfFTP::new(PositiveFloat::new(100.0).unwrap()),
+                )),
+            ],
+        )
+        .to_crm(),
+        "[COURSE HEADER]\n\
+    DESCRIPTION = test-1\n\
+    MINUTES PERCENTAGE\n\
+    [END COURSE HEADER]\n\
+    [COURSE DATA]\n\
+    0.00\t80.00\n\
+    5.00\t80.00\n\
+    5.00\t100.00\n\
+    10.00\t100.00\n\
+    [END COURSE DATA]"
+    )
+}
 
-// #[test]
-// fn simple_percent_of_ftp_workout_to_crm() {
-//     assert_eq!(
-//         Workout::new(
-//             "test_workout",
-//             "test-1",
-//             vec![
-//                 Effort::SingleEffort(EffortUnit::new(
-//                     PositiveFloat::new(5.0).unwrap(),
-//                     PercentOfFTP::new(
-//                         PositiveFloat::new(80.0).unwrap()
-//                     ),
-//                 )),
-//                 Effort::SingleEffort(EffortUnit::new(
-//                     PositiveFloat::new(5.0).unwrap(),
-//                     PercentOfFTP::new(
-//                         PositiveFloat::new(80.0).unwrap()
-//                     ),
-//                 )),
-//             ],
-//         )
-//         .to_crm(),
-//         "[COURSE HEADER]\n\
-//         DESCRIPTION = test-1\n\
-//         MINUTES PERCENTAGE\n\
-//         [END COURSE HEADER]\n\
-//         [COURSE DATA]\n\
-//         0.0	80.0\n\
-//         5.0	80.0\n\
-//         5.0	100.0\n\
-//         10.0	100.0\n\
-//         [END COURSE DATA]"
-//     )
-// }
+#[test]
+fn simple_watt_workout_to_crm() {
+    assert_eq!(
+        Workout::new(
+            "test_workout",
+            "test-1",
+            vec![
+                Effort::SingleEffort(EffortUnit::new(
+                    PositiveFloat::new(5.0).unwrap(),
+                    Watts::new(PositiveFloat::new(80.0).unwrap()),
+                )),
+                Effort::SingleEffort(EffortUnit::new(
+                    PositiveFloat::new(5.0).unwrap(),
+                    Watts::new(PositiveFloat::new(100.0).unwrap()),
+                )),
+            ],
+        )
+        .to_crm(),
+        "[COURSE HEADER]\n\
+        DESCRIPTION = test-1\n\
+        MINUTES WATTS\n\
+        [END COURSE HEADER]\n\
+        [COURSE DATA]\n\
+        0.00\t80.00\n\
+        5.00\t80.00\n\
+        5.00\t100.00\n\
+        10.00\t100.00\n\
+        [END COURSE DATA]"
+    )
+}
