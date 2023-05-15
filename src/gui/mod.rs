8e5122128fcac1c605cc26fcd231cc00e903d6bf
titoeb <titoebrock@googlemail.com>
2023-05-15 07:47:46 +0200

    feat: Remove workout definition.

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -2,10 +2,6 @@
 /// the mrc format.
 pub mod mrc_creator;
 
-/// The initial page of the WorkoutCreator on which
-/// summary information about the workout are entered.
-pub mod workout_definition;
-
 /// The second page of the WorkoutCreator on which
 /// the individual efforts of the workouts are designed.
 pub mod workout_design;
