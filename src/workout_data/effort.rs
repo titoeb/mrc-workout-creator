e8078bda50a6590164055d401c513ea941fa8098
titoeb <timtoebrock@gmail.com>
2025-09-25 11:44:56 +0200

    feat: Split ramp efforts into constant steps.
    The new roam v3 can't handle ramp efforts, at least in the mrc format, therefore, on save of the workout, now they will be transformed into individual steps.

--- src/workout_data/effort.rs
+++ src/workout_data/effort.rs
@@ -1,5 +1,13 @@
 use crate::workout_data::ToMRC;
 
+const SPLITTING_THRESHOLD_IN_MINUTES: f64 = 0.2;
+
+fn is_ramp_effort(effort: &Effort) -> bool {
+    effort.starting_value != effort.ending_value
+}
+fn effort_can_be_split(effort: &Effort) -> bool {
+    effort.duration_in_minutes > SPLITTING_THRESHOLD_IN_MINUTES
+}
 /// Combining a type of effort with a duration
 /// for which it should be executed.
 #[derive(Debug, Clone, PartialEq)]
@@ -37,8 +45,53 @@ impl Effort {
             gui_state: EffortState::default(),
         }
     }
+    // TODO: It would be better to have a seperate class `RampEffort` for this.
+    pub fn to_mrc_for_ramp_effort(&self, starting_minute: f64) -> String {
+        self.split_ramp_effort_into_constant_chunks()
+            .iter()
+            .fold(
+                (String::new(), starting_minute),
+                |(mrc_representation, starting_minute), effort| {
+                    let (mrc_representation_of_effort, starting_minute) =
+                        effort.to_mrc(starting_minute);
+                    (
+                        if mrc_representation.is_empty() {
+                            mrc_representation_of_effort
+                        } else {
+                            format!("{}\n{}", mrc_representation, mrc_representation_of_effort)
+                        },
+                        starting_minute,
+                    )
+                },
+            )
+            .0
+    }
+    pub fn split_ramp_effort_into_constant_chunks(&self) -> Vec<Effort> {
+        assert!(self.duration_in_minutes > 0.0);
+        assert!(self.starting_value != self.ending_value);
+
+        let steps = (self.duration_in_minutes / SPLITTING_THRESHOLD_IN_MINUTES).ceil() as usize;
+        let step_size = (self.ending_value - self.starting_value) / (steps as f64);
+        let mut result = Vec::with_capacity(steps);
+
+        for index in 0..steps {
+            let interpolated_effort_value =
+                self.starting_value + (index as f64 * step_size) + step_size / 2.0;
+
+            result.push(Effort {
+                duration_in_minutes: SPLITTING_THRESHOLD_IN_MINUTES,
+                starting_value: interpolated_effort_value,
+                ending_value: interpolated_effort_value,
+                gui_state: self.gui_state.clone(),
+            });
+        }
+        result
+    }
     pub fn to_mrc(&self, starting_minute: f64) -> (String, f64) {
         let end_of_effort = starting_minute + self.duration_in_minutes;
+        if is_ramp_effort(self) && effort_can_be_split(self) {
+            return (self.to_mrc_for_ramp_effort(starting_minute), end_of_effort);
+        }
         (
             format! {
                 "{}\t{}\n\
@@ -110,9 +163,9 @@ impl Effort {
 
 #[cfg(test)]
 mod tests {
-    use super::Effort;
+    use super::{effort_can_be_split, is_ramp_effort, Effort};
     mod effort_unit {
-        use super::Effort;
+        use super::{effort_can_be_split, is_ramp_effort, Effort};
         use crate::workout_data::workout::{efforts_to_mrc, extract_initial_starting_minutes};
         use crate::workout_data::ToMRC;
 
@@ -178,6 +231,69 @@ mod tests {
                     ),
                     40.0
                 )
+            )
+        }
+        #[test]
+        fn ramp_efforts_to_mrc() {
+            assert_eq!(
+                efforts_to_mrc(
+                    &vec![
+                        Effort::new(5.0, 100.0, None),
+                        Effort::new(1.0, 100.0, Some(150.0))
+                    ],
+                    5.0
+                ),
+                (
+                    String::from(
+                        "5.00\t100.00\n\
+                10.00\t100.00\n\
+                10.00\t105.00\n\
+                10.20\t105.00\n\
+                10.20\t115.00\n\
+                10.40\t115.00\n\
+                10.40\t125.00\n\
+                10.60\t125.00\n\
+                10.60\t135.00\n\
+                10.80\t135.00\n\
+                10.80\t145.00\n\
+                11.00\t145.00"
+                    ),
+                    11.0
+                )
+            )
+        }
+
+        #[test]
+        fn no_ramp_effort() {
+            assert!(!is_ramp_effort(&Effort::new(5.0, 100.0, None)));
+        }
+        #[test]
+        fn no_ramp_effort_explicit() {
+            assert!(!is_ramp_effort(&Effort::new(5.0, 100.0, Some(100.0))));
+        }
+        #[test]
+        fn ramp_effort() {
+            assert!(is_ramp_effort(&Effort::new(5.0, 100.0, Some(150.0))));
+        }
+        #[test]
+        fn effort_that_cannot_be_split() {
+            assert!(!effort_can_be_split(&Effort::new(0.1, 100.0, None)));
+        }
+        #[test]
+        fn effort_that_can_be_split() {
+            assert!(effort_can_be_split(&Effort::new(5.0, 100.0, Some(150.0))));
+        }
+        #[test]
+        fn split_effort_into_five_constant_pieces() {
+            assert_eq!(
+                Effort::new(1.0, 100.0, Some(150.0)).split_ramp_effort_into_constant_chunks(),
+                vec![
+                    Effort::new(0.2, 105.0, Some(105.0)),
+                    Effort::new(0.2, 115.0, Some(115.0)),
+                    Effort::new(0.2, 125.0, Some(125.0)),
+                    Effort::new(0.2, 135.0, Some(135.0)),
+                    Effort::new(0.2, 145.0, Some(145.0))
+                ]
             )
         }
     }
